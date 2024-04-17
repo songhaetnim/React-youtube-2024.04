694875dfffe0cd7c4da3f055463b04e8b27c221d
songhaetnim <minshinzz@naver.com>
2024-04-17 17:06:57 +0900

    관리자 기능

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,17 +1,19 @@
-
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, GithubAuthProvider,
+import { getAuth, createUserWithEmailAndPassword, GithubAuthProvider, GoogleAuthProvider,
   signInWithPopup, signOut, updateProfile, signInWithEmailAndPassword,
-  onAuthStateChanged } from "firebase/auth";
+  onAuthStateChanged, signInWithRedirect} from "firebase/auth";
+import { getDatabase, ref, get } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
+const database = getDatabase(app);
 
 export function register({ email, password, name, photo }) {
   console.log('firebase:register():', email, password);
@@ -25,7 +27,8 @@ export function register({ email, password, name, photo }) {
     .catch(console.error);
 }
 
-export function login({ email, password}) {
+export function login({ email, password }) {
+  console.log('firebase.js:login(): ', email, password);
   signInWithEmailAndPassword(auth, email, password)
     .catch(console.error);
 }
@@ -36,12 +39,46 @@ export function loginWithGithub() {
     .catch(console.error);
 }
 
+export function loginWithGoogle() {
+  const provider = new GoogleAuthProvider();
+  signInWithRedirect(auth, provider)
+  // signInWithPopup(auth, provider)
+    .catch(console.error);
+}
+
 export function logout() {
   signOut(auth).catch(console.error);
 }
 
 export function onUserStateChanged(callback) {
-  onAuthStateChanged(auth, (user) => {
-    callback(user);
+  onAuthStateChanged(auth, async (user) => {
+    const updatedUser = user ? await adminUser(user) : null;
+    callback(updatedUser);
   });
+}
+
+async function adminUser(user) {
+  return get(ref(database, 'admins'))
+    .then(snapshot => {
+      if (snapshot.exists()) {
+        const admins = snapshot.val();
+        console.log(admins);
+        const isAdmin = admins.includes(user.uid);
+        return {...user, isAdmin};
+      }
+      return user;
+    });
+}
+
+
+export async function getUserList() {
+  console.log('getUserList()');
+  return getAuth().listUsers(100)
+    .then(result => {
+      result.users.map(user => {
+        console.log(user.toJSON());
+      })
+      return result.users
+    })
+    .catch(console.error);
 }
